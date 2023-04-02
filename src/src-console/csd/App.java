00e7c5536999b72dca49d045e70d4e46bf77a61a
Oğuz Karan <oguzkaran@csystem.org>
2023-04-02 18:38:20 +0300

    Update

--- src/src-console/csd/App.java
+++ src/src-console/csd/App.java
@@ -1,8 +1,21 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Yorum satırları içerisinde yotum satırı oluşturmaya yarayan karakterlerin kullanımında dikkatli olunmalıdır. Aşağıdaki
+	örneği inceleyiniz
+-----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		
-	}
+        //Burası derleyici tarafından dikkate alınmayacaktır
+        System.out.println("Hello, World"); //Hello World printed
+                
+        /*
+            Burası da derleyici tarafından
+            /*dikkate alınmaz*/
+            
+        */ 
+    }
 }
+
+
