a381f8a9eb9bb9dc417c557665f064db43c20a79
Oğuz Karan <oguzkaran@csystem.org>
2023-04-16 15:14:59 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,20 +1,13 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örneği çalıştırarak yuvarlama hatalarını gözlemleyiniz
+	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		double a = 0.1, b = 0.2;
-		double c;
+	{
+		int _ = 10; //error		
 		
-		c = a + b;
-		
-		System.out.printf("a = %.20f%n", a);
-		System.out.printf("b = %.20f%n", b);
-		System.out.printf("c = %.20f%n", c);
-		
-    }
+	}
 }
 
