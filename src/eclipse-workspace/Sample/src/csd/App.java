9b3837faf4ba317fbba1c22bf5075c93485f2936
Oğuz Karan <oguzkaran@csystem.org>
2023-04-16 16:02:16 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,13 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
+	Java'da değişken isimlendirmede $ karakteri kullanılabilir. Hatta isimler $ karakteri ile başlatılabilir. $ karakteri
+	tek başına değişken ismi olabilir. Ancak programcı $ karakterini isimlendirmede kullanmamalıdır. 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		int _ = 10; //error		
+		int amountOf$s = 1000;
+		int $ = 10;
+		double $1234 = 3.4;
 		
+		
+		System.out.println(amountOf$s);
+		System.out.println($);
+		System.out.println($1234);		
 	}
 }
 
