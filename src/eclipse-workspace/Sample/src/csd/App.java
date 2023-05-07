3ce1c324e97df068d666b6aeb40f00a625b49547
Oğuz Karan <oguzkaran@csystem.org>
2023-05-07 17:06:42 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,16 +1,39 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	 Gerçek sayı sabitinin noktadan tam kısmı sıfır ise sıfır yazılmayabilir, ondalık kısmı sıfır ise yazılmayabilir.
-	 0.0 sabitinin yalnızca nokta olarak yazıolması geçersizdir	
+	Mantıksal Operatörler 3 tanedir: && (Logical AND), || (Logical OR), ! (Logical NOT).
+	
+	&& ve || operatörleri iki operandlı araek durumunda operatörlerdir. ! operatörü tek operandlı ve önek durumunda bir
+	operatördür. Bu operatörlerin operandları boolean türden olmak zorundadır. Bu operatörlerin ürettiği değerler de
+	boolean türdendir. Bu operatörler Matematikteki mantıksal operatörlere karşılık gelirler.
+	
+	Mantıksal operatörlerin doğruluk tabloları (truth tables):
+	
+	a				b				a && b					a || b
+  ------------------------------------------------------------------
+	T				T				  T						   T
+	T				F				  F						   T
+	F				T				  F						   T
+	F				F				  F						   F
+	
+	Yukarıdaki tabloya göre şu cümleler söylnebilir:
+	"Mantıksal AND işlemi operandlardan en az bir yanlışsa yanlıştır" 
+	"Mantıksal OR işlemi operandlardan en az bir doğruysa doğrudur"
+	
+	a				!a
+	T				F
+	F				T
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-		double a = 1.; //1.0
-		double b = .1; //0.1
-		
-		System.out.printf("a = %f, b = %f%n", a, b);
+	{		
+		NumberUtil.print(10, 20);
 	}
 }
 
+class NumberUtil {
+	public static void print(int a, int b)
+	{
+		System.out.printf("a = %d, b = %d%n", a, b);
+	}
+}
