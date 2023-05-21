575dcbe1b0b2fed675c8b0bdd167bf9602360146
Oğuz Karan <oguzkaran@csystem.org>
2023-05-21 21:55:59 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,12 +1,25 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	if deyimi:
+	Döngü değişkeninin döngüden sonraki değeri kullanılmayacaksa, 1.kısımda bildirilmesi daha iyi bir tekniktir. Bu durumda
+	kodu okuyan kişi döngü değişkeninin döngüden önce bildirildiğini görürse, bu değişkenin son değerinin döngüden sonra da
+	kullanıldığını algılar. Bu da kodun okunabilirliğini artırır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("Bir sayı giriniz:");
+		int n = kb.nextInt();		
+		
+		int i;
+		
+		for (i = 0; i < n; ++i)
+			System.out.printf("%d ", i);
+		
+		System.out.printf("Döngüden sonra i = %d%n", i);		
+		System.out.println("\nTekrar yapıyor musunuz?");		
 	}
 }
 
