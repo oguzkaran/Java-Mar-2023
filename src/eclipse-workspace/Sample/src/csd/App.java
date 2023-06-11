6cb5b9dd94a26cd5b92182279964733e19be8ff1
Oğuz Karan <oguzkaran@csystem.org>
2023-06-11 17:03:23 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,16 +1,25 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	İşlem Öncesi Otomatik Tür Dönüşümleri:
+	Tür Dönüştürme Operatörü (Type Cast Operator):  
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		boolean a = true;
-		int b;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		b = a; //error
+		while (true) {
+			System.out.print("İki sayı giriniz:");
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			double c;
+			
+			if (b == 0)
+				break;			
+			
+			c = (double)a / b;
+			
+			System.out.printf("%d / %d = %f%n", a, b, c);
+		}
 	}
 }
-
-
