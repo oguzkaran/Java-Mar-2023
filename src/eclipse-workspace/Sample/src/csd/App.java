8578519f39679f2dd9073c0e78dd415523fc5b13
Oğuz Karan <oguzkaran@csystem.org>
2023-06-24 21:47:13 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,48 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Bir Sınıf İçerisinde Aynı İsimde Birden Fazla Metot Yazılması: (Method Overloading)
+	Adres Kavramı:  
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{			
-		PrintCollatzTest.run();
-	}
-}
-
-class PrintCollatzTest {	
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		while (true) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-			
-			if (n == 0)
-				break;
-			
-			NumberUtil.printCollatz(n);
-		}
-		
-		System.out.println("Tekrar Yapıyor musunuz?");
-	}
-}
-
-class NumberUtil {	
-	public static void printCollatz(int n)
-	{
-		if (n <= 0) {
-			System.out.println("Geçersiz sayı!...");
-			return;
-		}
-		
-		System.out.println(n);
-		
-		while (n != 1) {
-			n = (n % 2 == 0) ? (n / 2) : (3  * n + 1);
-			System.out.println(n);
-		}	
 	}
 }
 
