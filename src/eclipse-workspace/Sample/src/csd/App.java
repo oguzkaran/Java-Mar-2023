c230a392022fc296b555081751faacf50fbb15cd
Oğuz Karan <oguzkaran@csystem.org>
2023-06-26 01:17:36 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,12 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Adres Kavramı:  
+    Aşağıdaki örnekte b'nin değeri ++a'dan sonra değişmez değil mi?
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{			
+	{		
+		int a = 20;
+		int b;
 		
+		b = a;
+		
+		++a;
+		
+		System.out.printf("a = %d, b = %d%n", a, b);				
 	}
+}
+
+class Sample {
+	public int a;
+	public boolean b;
+	
+	//...
 }
 
