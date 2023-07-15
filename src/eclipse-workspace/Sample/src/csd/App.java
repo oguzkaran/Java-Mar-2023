6fddf4c40b821fa41fd5b67c38a35f833edeea46
Oğuz Karan <oguzkaran@csystem.org>
2023-07-15 14:17:50 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,26 +1,51 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-    Aşağıdaki örnekte b'nin değeri ++a'dan sonra değişmez değil mi?
+    Sınıf Çalışması: İki zar atıldığında çift (ikisinin de aynı) gelme olasılığını yaklaşık olarak hesaplayan basit bir
+    simülasyon programını yazınız
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		int a = 20;
-		int b;
-		
-		b = a;
-		
-		++a;
-		
-		System.out.printf("a = %d, b = %d%n", a, b);				
+		CoinTailSimulationApp.run();				
 	}
 }
 
-class Sample {
-	public int a;
-	public boolean b;
-	
-	//...
+class CoinTailSimulationApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		while (true) {
+			System.out.print("Parayı kaç kez atmak istersiniz?");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n <= 0)
+				break;
+			
+			CoinTailSimulation cts = new CoinTailSimulation();
+			
+			cts.run(n);
+			
+			System.out.printf("Yazı gelme olasılığı:%f%n", cts.probability);
+		}	
+		
+	}
 }
 
+class CoinTailSimulation {
+	public double probability;
+	
+	public void run(int n)
+	{
+		java.util.Random r = new java.util.Random();
+		
+		int count = 0;
+		
+		for (int i = 0; i < n; ++i)
+			if (r.nextBoolean())
+				++count;
+		
+		probability = (double)count / n;
+	}	
+}
