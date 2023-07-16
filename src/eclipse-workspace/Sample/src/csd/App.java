17861e62cdc603742b8f419dc821f74c3c6e2c19
Oğuz Karan <oguzkaran@csystem.org>
2023-07-16 17:15:11 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	
+	charAt metoduna pozitif ya da negatif bakımdan sınırlar dışında bir indeksm numarası verildiğinde exception oluşur
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		String s = "ankara";
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.println(s);
+		System.out.print("Bir yazı giriniz:");
+		String s = kb.nextLine();
+		
+		char c = s.charAt(5);
+		
+		System.out.printf("c = %c%n", c);
 	}
 }
 
