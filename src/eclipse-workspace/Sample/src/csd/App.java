ff9ddbf906d76866e8b811c4aac8abfc48ec915b
Oğuz Karan <oguzkaran@csystem.org>
2023-07-22 15:02:33 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,45 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıfı Çalışması: Parametresi ile aldığı iki yazının birinci içerisinde ikincisinden kaç tane olduğu bilgisine
-	geri dönen countString isimli metodu StringUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
-	Örnek: xxx yazısı içerisinde xx 2 tane vardır
+	Length'i sıfır olan bir string'e boş string (empty string) denir. Boş string tipik olarak iki tane iki tırnak 
+	karakterinin bitişik olarak yazılması ile elde edilebilir. Şüphesiz String nesnesi yaratılmış olur
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		CountStringTest.run();
+		String s = "";
 		
+		System.out.printf("Length:%d", s.length());
 	}
 }
 
-class CountStringTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		while (true) {
-			System.out.print("Yazıyı giriniz:");
-			String s1 = kb.nextLine();
-			
-			if ("elma".equals(s1))
-				break;
-			
-			System.out.print("Aranacak yazıyı giriniz:");
-			String s2 = kb.nextLine();
-			
-			int count = StringUtil.countString(s1, s2);
-			
-			System.out.print("Sayı:%d%n", count);
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class StringUtil {
-	public static int countString(String s1, String s2)
-	{
-		//TODO:
-	}
-}
