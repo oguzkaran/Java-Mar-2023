7c49253e32e39e05bce8f8fcc0faa34e218aea18
Oğuz Karan <oguzkaran@csystem.org>
2023-07-22 14:04:27 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,19 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	charAt metoduna pozitif ya da negatif bakımdan sınırlar dışında bir indeksm numarası verildiğinde exception oluşur
+	Derleyici bir string literal'ın özdeğişini bir daha görürse, daha önce ilgili sabit için yaratılmış olan String
+	nesnesinin referansını verecek kodu üretir. Yani yeni bir String nesnesi yaratılmaz. 
+	Aşağıdaki örnekte yine referans karşılaştırması yapılmıştır. Referanslar aynı nesneyi gösterdiklerinden farklı
+	"Aynı yazı" ekrana basılır. Şüphesiz karşılaştırma gerçek yazı karşılaştırması değildir
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+		String s1 = "ankara";
+		String s2 = "ankara";
 		
-		System.out.print("Bir yazı giriniz:");
-		String s = kb.nextLine();
-		
-		char c = s.charAt(5);
-		
-		System.out.printf("c = %c%n", c);
+		System.out.println(s1 == s2 ? "Aynı yazı" : "Farklı yazılar");
+		System.out.println(s1 != s2 ? "Farklı yazılar" : "Aynı yazı");
 	}
 }
 
