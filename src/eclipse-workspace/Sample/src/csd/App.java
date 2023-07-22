b0cdd2027dea8d38348761b7a28426219273b578
Oğuz Karan <oguzkaran@csystem.org>
2023-07-22 14:33:07 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,19 +1,45 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Derleyici bir string literal'ın özdeğişini bir daha görürse, daha önce ilgili sabit için yaratılmış olan String
-	nesnesinin referansını verecek kodu üretir. Yani yeni bir String nesnesi yaratılmaz. 
-	Aşağıdaki örnekte yine referans karşılaştırması yapılmıştır. Referanslar aynı nesneyi gösterdiklerinden farklı
-	"Aynı yazı" ekrana basılır. Şüphesiz karşılaştırma gerçek yazı karşılaştırması değildir
+	Sınıfı Çalışması: Parametresi ile aldığı iki yazının birinci içerisinde ikincisinden kaç tane olduğu bilgisine
+	geri dönen countString isimli metodu StringUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	Örnek: xxx yazısı içerisinde xx 2 tane vardır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		String s1 = "ankara";
-		String s2 = "ankara";
+		CountStringTest.run();
 		
-		System.out.println(s1 == s2 ? "Aynı yazı" : "Farklı yazılar");
-		System.out.println(s1 != s2 ? "Farklı yazılar" : "Aynı yazı");
 	}
 }
 
+class CountStringTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		while (true) {
+			System.out.print("Yazıyı giriniz:");
+			String s1 = kb.nextLine();
+			
+			if ("elma".equals(s1))
+				break;
+			
+			System.out.print("Aranacak yazıyı giriniz:");
+			String s2 = kb.nextLine();
+			
+			int count = StringUtil.countString(s1, s2);
+			
+			System.out.print("Sayı:%d%n", count);
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+class StringUtil {
+	public static int countString(String s1, String s2)
+	{
+		//TODO:
+	}
+}
