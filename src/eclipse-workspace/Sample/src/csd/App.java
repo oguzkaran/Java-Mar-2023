90fb017a09b60d3c58424a14a3d0b3b6f4545a89
Oğuz Karan <oguzkaran@csystem.org>
2023-07-23 23:13:20 +0300

    Update App.java

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki programdaki ekran çıktısı ne olur?
+    
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		System.out.println("Java" + 20 + 23); //Java2023
-		System.out.println("Java" + (20 + 23)); //Java43		
-		System.out.println(20 + 23 + "Java"); //43Java
+		
 	}
 }
 
