222371307a904489f4fc77547a6feca2fcefa01f
Oğuz Karan <oguzkaran@csystem.org>
2023-07-29 17:11:01 +0300

    Update

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,35 +1,20 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	 Java 11 ile birlikte strip, stripLeading ve stringTrailin metotları eklenmiştir. strip metodu yazının başındaki 
-	 ve sonundaki boşluk (whitespace) karakterlerini, stripLeading yazının yalnızca başındaki boşluk karakterlerini ve
-	 stripTrailing ise yazının yalnızca sonundaki boşluk karakterlerini atar. strip metodunun trim metodundan farklı 
-	 bir örnekten sonra açıklanmaktadır
+/*----------------------------------------------------------------------------------------------------------------------	 
+	String sınıfının static format metodu, printf metodu gibi yazıyı formatlar ancak ekrana basmak yerine formatlanmış
+	yazıya ilişkin String referansına geri döner. Bu durumda programcı bu yazıyı dilediği gibi kullanabilir 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
-		StripLeadingTrailingTest.run();
-	}
-}
-
-class StripLeadingTrailingTest {	
-	public static void run()
-	{
+	{	
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-	
-		while (true) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.printf("(%s)%n", s.strip());
-			System.out.printf("(%s)%n", s.stripLeading());
-			System.out.printf("(%s)%n", s.stripTrailing());			
-		}
 		
-		System.out.println("Tekrar yapıyor musnuz?");
+		System.out.print("Birinci tamsayı, ikincisi gerçek sayı olarak iki tane sayı giriniz:");
+		int a = kb.nextInt();
+		double b = kb.nextDouble();
+		
+		String str = String.format("a = %02d, b = %.2f", a, b);
+		
+		System.out.println(str);
 	}
 }
