4efc35e58a9bc7ffafe5ee3ffa866049f5e1a8b3
Oğuz Karan <oguzkaran@csystem.org>
2023-08-06 17:01:46 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,13 @@
-/*----------------------------------------------------------------------------------------------------------------------	 
-	
+/*----------------------------------------------------------------------------------------------------------------------
+	Niteliksiz isim arama GENEL kuralları (else if biçiminde değerlendiriniz):
+	3. "import on demand declaration" varsa oradaki paketlerin hepsini bakılır. İleride ele alınacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello, World");
+
 	}
 }
 
