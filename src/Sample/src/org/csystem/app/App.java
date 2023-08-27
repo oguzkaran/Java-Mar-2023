6f99604479b9c304ab20070c21fa8ee9343d5004
Oğuz Karan <oguzkaran@csystem.org>
2023-08-27 15:56:56 +0300

    Update App.java

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı long türden bir sayının binerli olarak ayrılmış değerlerinden oluşan
-	bir diziye geri dönen digitsInThrees isimli NumberUtil sınıfı metodu yazınız ve aşağıdaki kod ile test ediniz
-	Açıklamalar:
-		- Örnekler:
-			1234567 sayısı için, 1 234 ve 567 değerlerinden oluşan 3 elemanlı bir diziye geri dönecektir
-			567 sayısı için, 567 değerinden oluşan 1 elemanlı bir diziye geri dönecektir
-		- NumberUtil sınıfı içerisinde genel testleri bozmaması koşuluyla istediğiniz değişikliği yapabilirsiniz
 
-		- Sayı negatif olsa bile elde edilen dizinin elemanları pozitif olacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
