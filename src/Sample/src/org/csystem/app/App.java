ab10128c69ce054ada79689023ddbf6131d83e2d
Oğuz Karan <oguzkaran@csystem.org>
2023-10-01 00:41:25 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -6,27 +6,7 @@ package org.csystem.app;
 class App {
 	public static void main(String [] args)
 	{
-		B b = new B();
 
-		b.bar(20);
-
-		System.out.printf("b.a = %d%n", b.a);
 	}
 }
 
-class B extends A {
-	//...
-	public void bar(int x)
-	{
-		a = x;
-		foo();
-	}
-}
-
-class A {
-	public int a;
-	public void foo()
-	{
-		System.out.println("A.foo");
-	}
-}
