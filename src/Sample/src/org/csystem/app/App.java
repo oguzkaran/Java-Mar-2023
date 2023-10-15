e906dc7333255dfebdd33285c27779117d5dc701
Oğuz Karan <oguzkaran@csystem.org>
2023-10-15 16:51:18 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,30 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	ArrayList sınıfının isEmpty metodu dizinin boş olup olmadığını test etmelk için kullanılır
+	Sınıf Çalışması: Kalvyeden quit girilene kadar alınan yazıları bir ArrayList içerisinde saklayınız. Bu işlemden sonra
+	bu ArrayList içerisinde yazıları aralarında - olacak şekilde birliştirilmiş yeni String elde ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 class App {
 	public static void main(String [] args)
 	{
-		ArrayList numbers = new ArrayList();
-		Random random = new Random();
-		int val;
 
-		while ((val = random.nextInt(0, 11)) != 0)
-			numbers.add(0, val);
-
-
-		if (!numbers.isEmpty()) {
-			for (Object o : numbers)
-				System.out.printf("%02d ", (int) o);
-
-			System.out.println();
-		}
-		else
-			System.out.println("No number generated!...");
 	}
 }
-
