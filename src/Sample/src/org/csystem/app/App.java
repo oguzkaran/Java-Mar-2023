9fb439e6acf8a1f61e76f940930e0c3082e8bcde
Oğuz Karan <oguzkaran@csystem.org>
2023-10-15 16:01:03 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,38 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Anımsanacağı gibi bir dizinin uzunlupu değiştirilemez. Öyleyse bir diziyi büyütmek için yeni bir dizi yaratılmalı,
-	eski dizinin elemanları yeni diziye kopyalanması ve referans eski diziden kopartılıp yeni diziyi gösterir duruma
-	getirilmelidir. Yani aslında bu durumda mantıksal olarak büyütülmiştür. Ancak burada dizi sürekli olarak, eklenecek
-	eleman sayısı kadar büyütülürse artık dizinin elemanlarına erişim O(n) karmaşıklıkta olur, bu da dizinin elemanlarına
-	sabit zamanlı erişme avantajını ortadan kaldırmış olur. İşte bu tip durumlarda dizi eklenecek eleman sayısı kadar değil
-	biraz daha fazla büyütülür. Böylelikle artık amortize edilmiş sabit zamanlı maliyetle çalışılmış olur. Yani dizinin
-	elemanlarına erişim avantajı çoğu zaman kullanılmış olur. İşte, dizileri gerektiğinde büyüten veri yapıları büyütme
-	işlemini bu şekilde yaparlar. Diziyi gerektiğinde büyütmek üzere tasarlanmış veri yapılarına "dynamic array" denir.
-	Bu tip veri yapılarında dizinin gerçek uzunluğuna "capacity", dizi içerisinde tutulan elemanların sayısına da
-	"size/count" denir. Buna göre aslında dinamik büyüyen dizi veri yapısında tutulan elemanlar size kadardır. Ancak
-	içsel olarak tutulan dizinin uzunluğu yani capacity değeri size'dan büyük veya size değerine eşit olabilir
+	ArrayList sınıfının isEmpty metodu dizinin boş olup olmadığını test etmelk için kullanılır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 class App {
 	public static void main(String [] args)
 	{
-		Sample.setX(20);
+		ArrayList numbers = new ArrayList();
+		Random random = new Random();
+		int val;
 
-		System.out.printf("Sample.x = %d%n", Sample.getX());
+		while ((val = random.nextInt(0, 11)) != 0)
+			numbers.add(0, val);
+
+
+		if (!numbers.isEmpty()) {
+			for (Object o : numbers)
+				System.out.printf("%02d ", (int) o);
+
+			System.out.println();
+		}
+		else
+			System.out.println("No number generated!...");
 	}
 }
 
-class Sample {
-	private static int x;
-
-	public static void setX(int x)
-	{
-		Sample.x = x;
-	}
-
-	public static int getX()
-	{
-		return x;
-	}
-
-}
