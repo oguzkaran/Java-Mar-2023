2346432e9e7be41bf4220565e15d7d678d7adaf5
Oğuz Karan <oguzkaran@csystem.org>
2023-10-22 13:57:39 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,37 +1,20 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	abstract bir ctor'u olabilir. Türemiş sınıf nesnesinin yaratılması aşamasında türemiş sınıfın ctor'undan önce
-	taban sınıfın ctor'unun çağrıldığını anımsayınız. Abstract sınıfların public ctor'larının olmasının mantıksal
-	olarak bir anlamı yoktur. Bu durumda ctor'un protected yapılması uygundur. Şüphesiz private veya no-modifier
-	ctor'ların sınıfın domain'i gereği anlamı olabilir
+	printf metodu da (dolayısıyla String sınıfının format metodu da) s format karakteri ile toString metodunu çağırarak
+	formatlama yapar
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.math.Complex;
+import org.csystem.math.geometry.Point;
 
 class App {
 	public static void main(String [] args)
 	{
+		Point p = Point.createCartesian(100, 200);
+		Complex c = new Complex(3, 4);
 
+		System.out.printf("Point:%s%n", p);
+		System.out.printf("Complex:%s%n", c);
 	}
 }
 
-class B extends A {
-	//...
-	public B(int x)
-	{
-		super(x);
-		//...
-	}
-
-	public void foo()
-	{
-		//...
-	}
-}
-
-abstract class A {
-	protected A(int a)
-	{
-		//...
-	}
-
-	public abstract void foo();
-}
