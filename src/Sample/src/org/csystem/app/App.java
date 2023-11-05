148c5c1787c340d8d93d0bdc073612c5acac9975
Oğuz Karan <oguzkaran@csystem.org>
2023-11-05 14:01:09 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,90 +1,77 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-	Try With Resources Statement:
+	Closeable Arayüzü ve Try With Resources Statement: Bazı kaynaklar (resources) kullanılmaya başlamadan önce bir takım
+	ilk işlemlerin yapılması gerekebilir. Bu işl işlemlerin yapılmasına mantıksal olarak "kaynağı açmak (open a resource)"
+	diyelim. Örneğin, bir resim dosyasının boyutlarını dosya içerisindeki verilerden almak isteyen bir program bu dosyayı
+	açmalıdır. Bu tip kaynakların kullanımı bittikten sonra da mantıksal olarak kapatılması yani kullanılan kaynağın
+	serbest bırakılması (release) gerekir. Hatta bu tip bazı kaynaklar yalnızca bir akış tarafından kullanılabilir. Aynı
+	anda birden fazla açılmaz. Bu durumda bu kaynakları kapatmak da daha kritik bir öneme sahiptir. İşte bu tarz kaynakları
+	kullanan sınıflar Java'da Closeable arayüzünü desteklerler. Dolayısıyla Java dünyasında Closeable arayüzünü destekleyen
+	bir sınıf yukarıdaki duruma benzer kaynakları kullanıyordur ve işlem bittiğinde bu kaynağın kapatılması gerekiyordur
+	anlaşılır. Closeable arayüzünün aşağıdaki protoipe sahip abstract bir close metodu vardır:
+
+		void close() throws IOException;
+
+	İlgili sınıf kaynağın mantıksal kapatılmasını yani ilgili kaynağın geri bırakılmasını bu metotta implemente eder.
+	Benzer şekilde yukarıdaki duruma benzer kaynak kullanan bir sınıf yazan programcı da bir contract/convention olarak
+	Closeable arayüzünü destekler ve kaynağı geri bırakma işlemini close metodunda yapar. Şüphesiz kaynağı kullanan
+	nesne yok edildiğinde kullandığı kaynak da geri bırakılır. Anca GC'nin ne zaman devreye belirsiz olduğundan kaynağın
+	nesnenin yok edilmesinden close edilmesi gerekebilir. Bu tarz kaynak kullanan sınıfları kaynağı mantık açma işlemini
+	genel olarak ctor'da yaparlar.
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.console.Console;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Random;
 
 class App {
-	public static void main(String [] args)
+	public static void main(String [] args) throws IOException
 	{
 
 	}
 }
 
-class Sample {
-	public static void doWork(int a) throws OurException
-	{
-		if (a % 2 == 0)
-			throw new OurException();
+class SensorInfo implements Closeable {
+	private String m_name;
+	private String m_host;
+	private int m_port;
+	private boolean m_open;
 
-		Console.writeLine("a = %d", a);
-	}
-}
-
-class D implements IX {
-	public void foo(int a) throws OurException
-	{
-		Sample.doWork(a);
-		//...
-	}
-}
-
-class C implements IX {
-	public void foo(int a) throws TheirException
-	{
-		//...
-	}
-}
-
-class B implements IX {
-	public void foo(int a) throws  MyException
-	{
-		//...
-	}
-}
-
-class A implements IX {
-	public void foo(int a)
-	{
-		//...
-	}
-}
-
-interface IX {
-	void foo(int a) throws Exception;
- }
-
- class WrapperException extends RuntimeException {
-	public WrapperException(String message)
-	{
-		this(message, null);
-	}
-
-	public WrapperException(String message, Throwable cause)
-	{
-		 super(message, cause);
-	}
-
-	public String getMessage()
-	{
-		Throwable cause = getCause();
-		return String.format("Message: %s%s", super.getMessage(), cause != null ? ", Cause Message:" + cause.getMessage() : "");
-	}
- }
-
-class MyException extends Exception {
 	//...
-}
 
-class YourException extends Exception {
-	//...
-}
+	public SensorInfo(String name, String host, int port) throws IOException
+	{
+		Random r = new Random();
 
-class TheirException extends MyException {
-	//...
-}
+		if (r.nextBoolean())
+			throw new IOException("Problem occured while opening!...");
 
-class OurException extends Exception {
-	//...
+		m_name = name;
+		m_host = host;
+		m_port = port;
+		m_open = true;
+		System.out.printf("%s sensor at %s:%d open!...%n", m_name, m_host, m_port);
+	}
+
+	public int read(byte [] data) throws IOException
+	{
+		Random random = new Random();
+
+		if (random.nextBoolean())
+			throw new IOException("Read problem occurred!...");
+
+		//...
+		System.out.printf("Read from %s sensor at %s:%d!...%n", m_name, m_host, m_port);
+
+		return data.length;
+	}
+
+	public void close() throws IOException
+	{
+		if (m_open)
+			System.out.printf("%s sensor at %s:%d closed!...%n", m_name, m_host, m_port);
+		else
+			System.out.print("Sensor connection already closed!...");
+	}
+
 }
