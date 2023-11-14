cce1e27901a71f8f7dbd1504a2a75144533fd304
Oğuz Karan <oguzkaran@csystem.org>
2023-11-14 23:33:26 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,29 +1,27 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-	Generic sınıflarda türetme durumu:
+	Generic bir sınıfın generic bir metodu da olabilmektedir. Buna member generic de denilmektedir
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
+		A<String> as = new A<>();
+		A<Integer> ai = new A<>();
 
+		as.foo("ankara", 10);
+		as.foo("ankara", true);
+		as.foo("ankara", 3.4);
+		ai.foo(20, 10);
+		ai.foo(20, true);
+		ai.foo(20, 3.4);
+		ai.<Float>foo(30, 3.4F);
 	}
 }
 
-class B implements IX<String, Integer> {
-	public void foo(String s, Integer i)
+class A<T> {
+	public <K> void foo(T t, K k)
 	{
 		//...
 	}
-}
-
-class A implements IX<Integer, String> {
-	public void foo(Integer i, String s)
-	{
-		//...
-	}
-}
-
-interface IX<T, K> {
-	void foo(T t, K k);
 }
