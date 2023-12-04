6d88d4b15e161df6208c0693b2b018c32f507f45
Oğuz Karan <oguzkaran@csystem.org>
2023-12-04 13:35:33 +0300

    Update App.java

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,8 +1,5 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-	Java'da generic tür parametresi türünden bir nesne ve dizi yaratmanın birden fazla yöntemi vardır. Bu kullanımda
-	uyarı mesajı alınacaktır. Uyarı mesajı önemsizdir. Bu yöntemlerin detayları Java ile Uygulama Geliştirme 1 ve 2
-	kurslarında "reflection" konusunda ele alınacaktır. Aşağıdaji örneği bir kalıp olarak kabul ederek inceleyiniz.
-	Uyarıları görmezden geliniz
+
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -11,25 +8,4 @@ class App {
 	{
 
 	}
-}
-class Sample<T> {
-	private T m_t;
-
-	public Sample()
-	{
-		m_t = (T)new Object();
-	}
-
-	//...
-}
-
-class Mample<T> {
-	private T [] m_t;
-
-	public Mample(int count)
-	{
-		m_t = (T[])new Object[count];
-	}
-
-	//...
 }
